b28a5011daa128953a8f64926071c9d95646fa07
JuanCabrera <juancabrera.it@gmail.com>
2016-01-19 14:44:55 +0100

    Minor fix

--- src/com/java/exe/TestRunner.java
+++ src/com/java/exe/TestRunner.java
@@ -11,11 +11,12 @@ public class TestRunner {
 	public static void main(String[] args) {
 		
 		Result result = JUnitCore.runClasses(TestJunit.class);
+		System.out.println("Found " + result.getFailureCount() + " errors");;
 		for(Failure failure : result.getFailures()) {
 			System.out.println(failure.toString());
 		}
 		
-		System.out.println("Test result: " + result.wasSuccessful());
+		System.out.println("Test result: " + result.wasSuccessful());		
 	}
 
 }
