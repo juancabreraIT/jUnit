37ae5ec3e55daea194dde89a6fb835aed71668d5
JuanCabrera <juancabrera.it@gmail.com>
2016-01-19 14:42:34 +0100

    Minor fix

--- src/com/java/exe/TestRunner.java
+++ src/com/java/exe/TestRunner.java
@@ -15,7 +15,7 @@ public class TestRunner {
 			System.out.println(failure.toString());
 		}
 		
-		System.out.println(result.wasSuccessful());
+		System.out.println("Test result: " + result.wasSuccessful());
 	}
 
 }
