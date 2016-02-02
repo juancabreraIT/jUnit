803dec94d57f801beee5be982e3bb65d75dd6f2d
JuanCabrera <juancabrera.it@gmail.com>
2016-02-02 14:48:31 +0100

    Employee Example

---
+++ src/employee/TestEmployeeDetails.java
@@ -0,0 +1,33 @@
+package employee;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class TestEmployeeDetails {
+
+	EmpBusinessLogic empBusinessLogic = new EmpBusinessLogic();
+	EmployeeDetails employee = new EmployeeDetails();
+	
+	@Test
+	public void testCalculateAppriasal() {
+		
+		employee.setName("Rajeev");
+		employee.setAge(25);
+		employee.setMonthlySalary(8000);
+		double appraisal = empBusinessLogic.calculateAppraisal(employee);
+		
+		assertEquals(500, appraisal, 0.0);
+	}
+	
+	@Test
+	public void testCalculateYearlySalary() {
+		
+		employee.setName("Rajeev");
+		employee.setAge(24);
+		employee.setMonthlySalary(8000);
+		double salary = empBusinessLogic.calculateYearlySalary(employee);
+		
+		assertEquals(96000, salary, 0.0);
+	}
+}
