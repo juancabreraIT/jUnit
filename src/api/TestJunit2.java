b56b6347743781a2511133f6dd5de901fbbcf29b
JuanCabrera <juancabrera.it@gmail.com>
2016-02-02 10:04:40 +0100

    [API] - TestCase class

---
+++ src/api/TestJunit2.java
@@ -0,0 +1,36 @@
+package api;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import junit.framework.TestCase;
+
+public class TestJunit2 extends TestCase {
+	
+	protected double fValue1;
+	protected double fValue2;
+	
+	@Before
+	public void setUp() {
+		fValue1 = 2.0;
+		fValue2 = 3.0;
+	}
+
+	@Test
+	public void testAdd() {
+		
+		System.out.println("No of Test Case = " + this.countTestCases());
+		
+		String name = this.getName();
+		System.out.println("Test Case Name = " + name);
+		
+		this.setName("testNewAdd");
+		String newName = this.getName();
+		System.out.println("Updated Test Case Name = " + newName);
+		
+		int value = 12;
+		assertEquals(12, value);
+	}
+	
+	public void tearDown() { }
+}
