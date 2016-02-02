9d5629093d91ad007f9e6c0cfd6ea98d223b23ef
JuanCabrera <juancabrera.it@gmail.com>
2016-02-02 14:28:37 +0100

    [API] - Test Suite class

---
+++ src/api/JunitTestSuite.java
@@ -0,0 +1,18 @@
+package api;
+
+import junit.framework.TestResult;
+import junit.framework.TestSuite;
+
+public class JunitTestSuite {
+
+	public static void main(String[] a) {
+		
+		TestSuite suite = new TestSuite(TestJunit1.class, TestJunit2.class, TestJunit3.class);
+		TestResult result = new TestResult();
+		
+		suite.run(result);
+		
+		System.out.println("Number of test cases: " + result.runCount());
+	}
+	
+}
