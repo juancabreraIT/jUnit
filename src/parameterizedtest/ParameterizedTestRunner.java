358eeb69c00b6b0cac1d27666de0f3a2f32f51cf
JuanCabrera <juancabrera.it@gmail.com>
2016-02-03 12:25:09 +0100

    Parameterized Test

---
+++ src/parameterizedtest/ParameterizedTestRunner.java
@@ -0,0 +1,21 @@
+package parameterizedtest;
+
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
+
+public class ParameterizedTestRunner {
+
+	public static void main(String[] args) {
+		
+		Result result = JUnitCore.runClasses(PrimeNumberCheckerTest.class);
+		
+		for(Failure failure : result.getFailures()) {
+			System.out.println(failure.toString());
+		}
+		
+		System.out.println("\nTests run: " + result.getRunCount());
+		System.out.println("Tests failed: " + result.getFailureCount());
+	}
+
+}
